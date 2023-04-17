fac531efe0b4e898e8699a8d416876753b7f5aaa
Kyszty <patrickcasimiroz@gmail.com>
2023-04-17 18:28:30 -0300

    atualization

--- Challenges/Challanger05/js/script.js
+++ Challenges/Challanger05/js/script.js
@@ -49,7 +49,7 @@ a.appendChild(li);
 
 
 
-/*-*-*-*-************************ Main *************************/
+/************************** Main Carrosel***********************/
 
 function carrosel() {
 
@@ -214,7 +214,7 @@ function carrosel() {
       
           case 5:
               img03.innerHTML = '<img src="https://cdn.fansshare.com/photograph/italianpizza/tasty-pizza-wallpaper-hd-wallpapers-wallpaper-1750945969.jpg">'
-              break3
+              break;
   
           case 6:
               img03.innerHTML = '<img src="../img/hamburguer/pexels-photo-14710227.jpeg">'
@@ -355,28 +355,259 @@ function slideInterval() {
 
     if (seconds >= 0 && seconds <= 9) {
         document.querySelector('#slide1').checked = true;
-
+        bg_check();
     } else if (seconds >= 10 && seconds <= 19) {
         document.querySelector('#slide2').checked = true;
+        bg_check();
     } else if (seconds >= 30 && seconds <= 39) {
         document.querySelector('#slide3').checked = true;
+        bg_check();
     } else if (seconds >= 40 && seconds <= 49) {
         document.querySelector('#slide4').checked = true;
+        bg_check();
     } else if (seconds >= 50 && seconds <= 59) {
         document.querySelector('#slide5').checked = true;
+        bg_check();
     }
-    console.log(seconds)
+   
 }
 
 window.setInterval('slideInterval()', 10000);
+
+
+function leftSlide() {
+  const check01 = document.querySelector('#slide1')
+  const check02 = document.querySelector('#slide2')
+  const check03 = document.querySelector('#slide3')
+  const check04 = document.querySelector('#slide4')
+  const check05 = document.querySelector('#slide5')
+
+
+  if (check01.checked === false && check02.checked === false && check03.checked === false && check04.checked === false && check05.checked === false) {
+      check05.checked = true
+    } else if (check05.checked === true) {
+      check04.checked = true
+      
+    } else if (check04.checked === true) {
+      check03.checked = true
+    } else if (check03.checked === true) {
+      check02.checked = true
+    } else if (check02.checked === true) {
+      check01.checked = true
+    } else if (check01.checked === true) {
+      check05.checked = true
+    }
+
+}
    
+function rightSlide() {
+    const check01 = document.querySelector('#slide1')
+    const check02 = document.querySelector('#slide2')
+    const check03 = document.querySelector('#slide3')
+    const check04 = document.querySelector('#slide4')
+    const check05 = document.querySelector('#slide5')
+
+  console.log(check01.checked, check02.checked, check03.checked, check04.checked, check05.checked)
+
+  if (check01.checked === false && check02.checked === false && check03.checked === false && check04.checked === false && check05.checked === false) {
+    check02.checked = true
+   
+  } else if (check02.checked === true) {
+    check03.checked = true
+  } else if (check03.checked === true) {
+    check04.checked = true
+   
+  } else if (check04.checked === true) {
+    check05.checked = true
+   
+  } else if (check05.checked === true) {
+    check01.checked = true
+   
+  } else if (check01.checked === true) {
+    check02.checked = true
+  }
+
+
+   
+  
+    
+}
+
+function bg_check() {
+  slide1.checked? labelToSlide01.style.backgroundColor = 'var(--cor01)': labelToSlide01.style.backgroundColor = '';
+
+  slide2.checked? labelToSlide02.style.backgroundColor = 'var(--cor01)': labelToSlide02.style.backgroundColor = '';
+
+  slide3.checked? labelToSlide03.style.backgroundColor = 'var(--cor01)': labelToSlide03.style.backgroundColor = '';
+
+  slide4.checked? labelToSlide04.style.backgroundColor = 'var(--cor01)': labelToSlide04.style.backgroundColor = '';
+
+  slide5.checked? labelToSlide05.style.backgroundColor = 'var(--cor01)': labelToSlide05.style.backgroundColor = '';
+
+}
+
+bg_leftArrow.addEventListener('click', function() {
+    leftSlide(), bg_check();
+});
+
+bg_rightArrow.addEventListener('click',function() {
+    rightSlide(), bg_check();
+});
+
+
+
+
+/*--------------------------Main Combo Food-----------------------*/
+
+const boxLeft = document.querySelector('.box-left');
+
+
+const boxRight = document.querySelector('.box-right');
+
+boxLeft.animate([{transform: 'rotate(0deg)'}, {transform: 'rotate(360deg)'}, {top: '87%'}], {duration: 2000, easing: 'linear', iterations: 1 });
+
+boxRight.animate([{transform: 'rotate(0deg)'}, {transform: 'rotate(360deg)'}, {top: '87%'}], {duration: 2000, easing: 'linear', iterations: 1 });
+
+
+var timerx2 = setInterval(() => {
+
+    if (boxLeft.getBoundingClientRect().top >= 450 && boxRight.getBoundingClientRect().top >= 450) {
+        boxLeft.style.top = 705 + 'px';
+        boxRight.style.top = 705 + 'px';
+        boxLeft.style.transform = 'scale(0.5)';
+        boxRight.style.transform = 'scale(0.5)';
+     
+    };
+
+    
+
+    const arrowLeft = document.querySelector('.arrow-left');
+
+    const arrowRight = document.querySelector('.arrow-right');
+
+    arrowLeft.style.opacity = 100;
+
+    arrowRight.style.opacity = 100;
+
+    boxLeft.style.rotate = '135deg';
+
+    boxLeft.animate([{left: '38%'}],{duration: 1000, easing: 'linear' , iterations: 1});
+
+    boxRight.animate([{left: '57%'}],{duration: 1000, easing: 'linear' , iterations: 1});
+   
+    var count = 0;
+    var id = 0;
+
+   
+
+    const text = ' Combo Food of the Daily';
+    const listText = [];
+
+    text.split('').forEach((add) => listText.push(add));
+
+    const boxCenter = document.createElement('div');
+    boxCenter.className = 'box-center';
+
+    const subDivCF = document.createElement('div');
+    subDivCF.className = 'sub-div-cf';
+
+    const comboFood = document.querySelector('.combo-food');
+
+    boxCenter.appendChild(subDivCF);
+    comboFood.appendChild(boxCenter);
+
+    var countTimer = setInterval(() => {
+        if (count < 23 ) {
+            count++;
+            subDivCF.innerHTML += listText[count];
+            return;
+        }
+        clearInterval(id);
+    }, 50);
+
+    id = setInterval(countTimer);
+
+// limpa o timer do próprio bloco.
+   clearInterval(timerx2);
+
+}, 1900);
+
+
+
+
+const timerX3 = setInterval(() => {
+    const boxCenter =  document.querySelector('.box-center');
+
+    const boxRight = document.querySelector('.box-right');
+
+    const comboFood = document.querySelector('.combo-food');
+
+    comboFood.insertBefore(boxCenter, boxRight);
+    
+    const boxBefore = document.createElement('div');
+    boxBefore.className = 'sub-box-before';
+
+
+   let locatization = boxCenter.getBoundingClientRect().left;
+   let menos = 50;
+
+   let result = locatization - menos;
+   let resultX2 = locatization + 185;
+
+    boxLeft.style.left = result + 'px';
+    boxRight.style.left = resultX2 + 'px';
+
+    const boxLeftCenter = document.createElement('div');
+    boxLeftCenter.className = 'box-left-center';
+
+    const boxRightCenter = document.createElement('div');
+    boxRightCenter.className = 'box-right-center';
+
+    comboFood.appendChild(boxLeftCenter);
+
+    comboFood.insertBefore(boxLeftCenter, boxCenter);
+
+    comboFood.appendChild(boxRightCenter);
+
+    comboFood.insertBefore(boxRightCenter, boxRight);
+
+    // animation
+
+  
+    let time = 0;
+    let id = 0;
+
+    let timerX5 = setInterval(() => {
+        let screenWidth = window.innerWidth;
+        let newScreenWidth = screenWidth / 3.5;
+
+        const boxLeftCenter = document.querySelector('.box-left-center');
+
+        if (time <= newScreenWidth) {
+           time++;
+           boxLeftCenter.style.width = time + 'px';
+           boxRightCenter.style.width = time + 'px';
+           console.log(boxLeftCenter.style.width);
+           return;
+        };
+        boxCenter.innerHTML = newScreenWidth
+
+        clearInterval(id)
+    }, 1);
+
+    id = timerX5;
+
+    clearInterval(timerX3);
+}, 2800);
+
+
 /*-----------------------Footer----------------------*/
 
 body.addEventListener('mousewheel', function () {
-  m()
+  backTop();
 })
 
-function m() {
+function backTop() {
   if (window.scrollY > 500) {
     document.querySelector('#backTop').style.bottom = 0
     document.querySelector('#backTop').style.transform = 'translateY(0%)'
